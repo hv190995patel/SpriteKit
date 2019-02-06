828b33bec3a274dbd5ff92182d050439e306bdd7
MacStudent <macstudent@126-iMacXX-M.local>
2019-02-06 11:48:53 -0500

    Created initial template for SpriteKit games

--- SpriteKitIntro/GameViewController.swift
+++ SpriteKitIntro/GameViewController.swift
@@ -15,22 +15,9 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
+        
+        
+     }
 
     override var shouldAutorotate: Bool {
         return true
