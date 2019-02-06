7218c9a6a1b25e8cde184057f0720c766a5c48a9
MacStudent <macstudent@126-iMacXX-M.local>
2019-02-06 12:12:27 -0500

    Add text to the screen and position it at (200, 250)

--- SpriteKitIntro/GameScene.swift
+++ SpriteKitIntro/GameScene.swift
@@ -11,7 +11,31 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    /*
+     What are things you can draw on a canvas?
+     - drawRect
+     - drawCirc
+     - canvas.drawText("HELLO WORLD", 100, 50, paintbrush)
+     - drawBitmaps
+     - drawLine
+     
+     - setColor
+     - setTextSize
+     - setStyle (FILL, STROKE)
+    */
+    
+    
+    // Example 1  - Adding text to the screen
+    let label = SKLabelNode(text:"HELLO WORLD!")
+    
     override func didMove(to view: SKView) {
+        // configure your text
+        label.position = CGPoint(x:200,y:250);
+        label.fontSize = 45
+        label.fontColor = SKColor.yellow
+        
+        // add it to your scene (draw it!)
+        addChild(label)
         
     }
     
